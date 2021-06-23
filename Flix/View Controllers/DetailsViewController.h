93f1c0538af3c77c4a27e803a2ea4539b68b5347
Sarah Wang <sarah.wang2009@gmail.com>
2021-06-23 18:27:50 -0400

    Movie details view

---
+++ Flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.h
+//  Flix
+//
+//  Created by Sarah Wang on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+@end
+
+NS_ASSUME_NONNULL_END
--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -8,6 +8,7 @@
 #import "MoviesViewController.h"
 #import "MovieCell.h"
 #import "UIImageView+AFNetworking.h"
+#import "DetailsViewController.h"
     //adding additional helper methods to UIImageView that didn't exist before
 
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
@@ -31,7 +32,8 @@
     
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
-    [self.tableView insertSubview:self.refreshControl atIndex:0];         //add to table view
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+         //add to table view
 }
 
 - (void)fetchMovies {
@@ -90,14 +92,20 @@
     [cell.posterView setImageWithURL:posterURL];
     return cell;
 }
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
+    UITableViewCell *tappedCell = sender;
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:tappedCell];
+    NSDictionary *movie = self.movies[indexPath.row];
+    
+    DetailsViewController *detailsViewController = [segue destinationViewController];
+    detailsViewController.movie = movie;
+    NSLog(@"Tapping on a movie!");
+    // Get the new view controller using [segue destinationViewController]. Here, destination is the movie that we tapped on.
     // Pass the selected object to the new view controller.
 }
-*/
 
 @end
