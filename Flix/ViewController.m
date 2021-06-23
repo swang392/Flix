d6d9fee69b06b8dbf2dad4efcd7aaf3c607002fe
Sarah Wang <sarah.wang2009@gmail.com>
2021-06-23 11:49:43 -0400

    Initial Commit

---
+++ Flix/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Flix
+//
+//  Created by Sarah Wang on 6/23/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
